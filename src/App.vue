103e96214ed5ad86a4be65f96e820fd9f0fcf5cb
quentinquek <quentinquek@hotmail.com>
2023-08-17 22:31:16 +0800

    Add: Tailwind + Vue.js
    Setup

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div>
+    <HelloWorld msg="Vite + Vue" />
+  </div>
+</template>
+
+<style scoped></style>
